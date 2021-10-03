3d594c093df4b3c5b7dae36719e2d07598c546b4
krishlala <71400038+krishlala@users.noreply.github.com>
2021-10-03 16:19:59 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+img = "";
+status = "";
+objects = [];
+
+function setup() {
+    canvas = createCanvas(500, 400);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function preload() {
+    img = loadImage('dog_cat.jpg')
+}
+
+function modelLoaded() {
+    console.log("ModelLoaded! is intialized");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function draw() {
+    image(img, 0, 0, 500, 400);
+    if(status != "") {
+        for (i=0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            fill('red');
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x +15, objects[i].y +15);
+            noFill();
+            stroke('red');
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+        }
+
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        objects = results;
+    }
+}
